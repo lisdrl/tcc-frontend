37d8693dd84274f9c6ccbcd5af00d6f68eb7756e
Lis Andreolo <lisandre.darioli@gmail.com>
2025-06-08 09:59:59 -0300

    feat: Use env variable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider';
 import { Home } from './pages/Home';
 import GlobalStyles from './styles/GlobalStyles';
+import { SENDBIRD_USER_ID } from './features/users/constants';
+import '@sendbird/uikit-react/dist/index.css';
+
+const SENDBIRD_APP_ID = import.meta.env.VITE_SENDBIRD_APP_ID ?? '';
 
 export const App: React.FC = () => {
   return (
     <>
-      <GlobalStyles />
-      <Home />
+      <SendbirdProvider appId={SENDBIRD_APP_ID} userId={SENDBIRD_USER_ID} accessToken='135fbbab8c6125b1c994ddd9d4ff877ec6dde57b'>
+        <GlobalStyles />
+        <Home />/
+      </SendbirdProvider>
     </>
   );
 };
